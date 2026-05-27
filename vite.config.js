4a4f70a984b2a78b8b91391ecbbc856114a3da3d
kevinsepp63-svg <kevinsepp63@gmail.com>
2026-05-27 09:40:41 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  root: __dirname,
+  base: '/',
+  server: {
+    port: 4173,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
+})
