e80b94029cf64b80921b1300848b3ed636020227
kevinsepp <kevinsepp@example.com>
2026-05-27 09:28:31 +0300

    Initial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const app = document.querySelector('#app')
+
+if (app) {
+  app.innerHTML = `
+    <main>
+      <h1>Tere Tulemast Vite'i!</h1>
+      <p>See on Vite projekti algus.</p>
+    </main>
+  `
+}
+
